2d193394fa8d056b1de0b31c4ef17a067cd1a2f8
Zero <i@yikzero.com>
2021-02-04 00:38:04 +0800

    需指定Edge驱动地址

--- daka.py
+++ daka.py
@@ -19,7 +19,7 @@ def work():
 	try:
 		#打开浏览器
 		browser = webdriver.Chrome()  #如使用的是Edge 请注释
-		#browser = webdriver.Edge()
+		#browser = webdriver.Edge(r'驱动地址，例：D:\Download\Clock-in-master\msedgedriver.exe')
 		browser.get(url)
 		#输入python
 		browser.find_element_by_id('username').clear()
