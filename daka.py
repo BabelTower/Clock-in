0470ddfca943ddaa608317f0fb81e356439792f2
Zero <i@yikzero.com>
2021-02-04 15:47:47 +0800

    使用implicitly_wait方法加快自动化运行

--- daka.py
+++ daka.py
@@ -4,22 +4,22 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from apscheduler.schedulers.blocking import BlockingScheduler
-import key
  
 #打卡时间 小时、分钟
-c_minute = "45" 
-c_hour = "16"
+c_minute = "30" 
+c_hour = "8"
 
 #网址、用户名、密码
 url = 'http://mapp.zjut.edu.cn/_web/_apps/eform/onlinesurvey/onlineSurvey_m.jsp?domainId=1&topicId=17'  #学生战役网址
-username = key.username
-password = key.password
+username = '学号'
+password = '密码'
 
 def work():
 	try:
 		#打开浏览器
 		browser = webdriver.Chrome()  #如使用的是Edge 请注释
 		#browser = webdriver.Edge(r'驱动地址，例：D:\Download\Clock-in-master\msedgedriver.exe')
+		browser.implicitly_wait(10) #使用implicitly_wait来指定最大等待时间，取消使用sleep
 		browser.get(url)
 		#输入python
 		browser.find_element_by_id('username').clear()
@@ -28,16 +28,15 @@ def work():
 		browser.find_element_by_id('password').send_keys(password) #password
 		browser.find_element_by_class_name('sl-login-btn').click()
 		browser.find_element_by_class_name('l-btn-text').click()
-		time.sleep(2)
 		browser.find_element_by_link_text('提交').click()
 		browser.find_element_by_class_name('l-btn-text').click()
 	
 		#退出浏览器
-		time.sleep(2)
 		browser.quit()
-		print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())+": Clock Success!")
 	except:
 		print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())+": Clock Filed!")
+	else:
+		print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())+": Clock Success!")
 
 if __name__ == '__main__':
 	#添加任务
